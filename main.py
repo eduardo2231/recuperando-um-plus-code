9566a7c9e270f135b59db5d816adfa44678ab6c4
Eduardo B. <bonomettieduardo2@gmail.com>
2025-07-03 21:20:34 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import urllib.request, urllib.parse
+import json, ssl
+
+# Heavily rate limited proxy of https://www.geoapify.com/ api
+serviceurl = 'https://py4e-data.dr-chuck.net/opengeo?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    address = address.strip()
+    parms = dict()
+    parms['q'] = address
+
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+    # print(json.dumps(js, indent=4))
+    # University of Athens
+    print(js["features"][0]["properties"]["plus_code"])
